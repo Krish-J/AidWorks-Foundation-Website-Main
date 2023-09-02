c89ce2cbdedafaeef3fc11aaf76a969b4f41fd1a
Krish-J <krishj718@gmail.com>
2023-09-02 02:09:34 -0400

    fixed positioning

--- src/components/pages/donatePage/donateMain.js
+++ src/components/pages/donatePage/donateMain.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "../../../App.css";
 import Donate from "./donate.js";
+import DonateNav from "./donateNav";
+import "./donateMain.css";
 
 
 function DonateMain(){
     return(
         <>
+        <DonateNav/>
         <Donate/>
         
         </>
