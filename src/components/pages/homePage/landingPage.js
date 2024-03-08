2fcdbd4827139b36eef3458597dbd4bf1da3a037
Krish-J <krishj718@gmail.com>
2024-03-08 02:07:02 -0500

    update comment on landingPage

--- src/components/pages/homePage/landingPage.js
+++ src/components/pages/homePage/landingPage.js
@@ -6,7 +6,7 @@ function LandingPage() {
     return (
         <div className="landingParent">
             <div className="landingContent">
-                //*<h1 className="landingTitle">For The People</h1>*//
+                {/* <h1 className="landingTitle">For The People</h1> */}
                 <h1 className="landingTitle">Compassion Creates Change</h1>
                 <button className="landDonate">
                 <a target="_blank" rel="noopener" href="https://docs.google.com/forms/d/e/1FAIpQLSc9_Qu3yH-YX5PXIsMJIWdHeNLmLZBP6v7hZmFe2ypopmPG0Q/viewform" className="landingButton"> Volunteer Now!</a>
