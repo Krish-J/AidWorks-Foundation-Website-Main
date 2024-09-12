4134e7a67e2b7a1589df87e5d008e8174e3b30a4
ArnavTheSharma <arnavsharma06@gmail.com>
2024-09-12 12:17:03 -0400

    Cleaned ContactJS Code

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './components/pages/homePage/Home.js';
 import DonateMain from './components/pages/donatePage/donateMain.js';
 import Footer from './components/footer.js';
 import OurTeamMain from './components/pages/ourTeamPage/ourTeamMain.js';
-import ContactMain from './components/pages/contactPage/contactMain.js';
+import Contact from './components/pages/contactPage/contact.js';
 import BlogMain from './components/pages/blogPage/blogMain.js';
 import ScrollToTop from './components/ScrollToTop.js';
 import AidWorks5k from './components/pages/5kPage/aidWorks5kMain.js';
@@ -21,7 +21,7 @@ function App() {
           <Route exact path='/' element={<Home/>}/>
           <Route exact path='/donate' element={<DonateMain/>}/>
           <Route exact path='/ourteam' element={<OurTeamMain/>}/>
-          <Route exact path='/contact' element={<ContactMain/>}/>
+          <Route exact path='/contact' element={<Contact/>}/>
           <Route exact path='/blog' element={<BlogMain/>}/>
         </Routes>
         <Footer/>
